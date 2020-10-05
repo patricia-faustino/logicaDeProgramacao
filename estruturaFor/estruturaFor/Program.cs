b75ab47c31d5984234730746af45001e82f962a3
Patricia <patriciafaustinobr@gmail.com>
2020-10-05 20:09:36 -0300

    Adicionei operacao soma ao exercicio de estrutura de repeticao for

--- estruturaFor/estruturaFor/Program.cs
+++ estruturaFor/estruturaFor/Program.cs
@@ -8,12 +8,14 @@ namespace estruturaFor
         {
             Console.Write("Quantos números inteiros você vai digitar? ");
             int n = int.Parse(Console.ReadLine());
+            int sum = 0;
             for(int i = 1; i <= n; i++)
             {
                 Console.Write("Valor #" + i + ": ");
                 int dig = int.Parse(Console.ReadLine());
-                
+                sum += dig;
             }
+            Console.WriteLine("Soma: " + sum);
         }
     }
 }
