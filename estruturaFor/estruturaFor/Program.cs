570028edfd849ec9a95c5b3a19eccc35bc14d048
Patricia <patriciafaustinobr@gmail.com>
2020-10-05 20:06:55 -0300

    Exercicio com estrutura de repeticao for

---
+++ estruturaFor/estruturaFor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace estruturaFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= n; i++)
+            {
+                Console.Write("Valor #" + i + ": ");
+                int dig = int.Parse(Console.ReadLine());
+                
+            }
+        }
+    }
+}
